a916573709d82f4ad24681eacb817c8def8c64ba
Ginnie Richichi <ginnie.richichi@hotmail.co.uk>
2021-02-05 11:44:18 +0000

    Functional Search Bar

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -8,17 +8,11 @@ use Livewire\Component;
 class SearchDropdown extends Component
 {
     public $search = '';
-    public $selectedContactId= null;
 
     public function selectContact($id)
     {
         $this->selectedContactId = $id;
         $this->search = '';
-    }
-
-    public function clearSelection()
-    {
-        $this->selectedContactId = null;
     }
 
     public function render()
@@ -27,7 +21,6 @@ class SearchDropdown extends Component
 
         return view('livewire.search-dropdown', [
             'contacts' => $contacts,
-            'selectedContact' => User::find($this->selectedContactId),
         ]);
     }
 }
--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,13 +1,6 @@
 <div class="relative mt-3">
     <div class="flex items-center">
-        @unless($selectedContact)
         <x-input.text type="text" wire:model.debounce.duration.500ms="search" placeholder="&#xf002;  Search...." />
-        @else
-            <div class="flex justify-between items-center">
-                <div class="pr-6">{{ $selectedContact->name }}</div>
-                <x-button.link wire:click="clearSelection"><i class="fas fa-times"></i></x-button.link>
-            </div>
-        @endunless
     </div>
 
     @if($search)
@@ -15,10 +8,10 @@
         <ul>
             @forelse ($contacts as $contact)
             <li class="border-b border-gray-300">
-                <x-button.link wire:click="selectContact({{ $contact->id }})"
+                <a href="{{ route('profile', $contact->id) }}"
                                class="text-left block hover:bg-gray-100 w-full p-3">
                     {{ $contact->name }}
-                </x-button.link>
+                </a>
             </li>
             @empty
                 <li class="border-b border-gray-300">
