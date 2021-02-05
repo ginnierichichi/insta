d50973281fa257c52c3d31bfebf10ca7c82a0acd
Ginnie Richichi <ginnie.richichi@hotmail.co.uk>
2021-02-05 11:37:46 +0000

    Unnecessary button

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -8,11 +8,26 @@ use Livewire\Component;
 class SearchDropdown extends Component
 {
     public $search = '';
+    public $selectedContactId= null;
+
+    public function selectContact($id)
+    {
+        $this->selectedContactId = $id;
+        $this->search = '';
+    }
+
+    public function clearSelection()
+    {
+        $this->selectedContactId = null;
+    }
 
     public function render()
     {
+        $contacts = User::search('name', $this->search)->get();
+
         return view('livewire.search-dropdown', [
-            'contacts' => User::search('name', $this->search),
+            'contacts' => $contacts,
+            'selectedContact' => User::find($this->selectedContactId),
         ]);
     }
 }
--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,20 +1,31 @@
 <div class="relative mt-3">
     <div class="flex items-center">
+        @unless($selectedContact)
         <x-input.text type="text" wire:model.debounce.duration.500ms="search" placeholder="&#xf002;  Search...." />
+        @else
+            <div class="flex justify-between items-center">
+                <div class="pr-6">{{ $selectedContact->name }}</div>
+                <x-button.link wire:click="clearSelection"><i class="fas fa-times"></i></x-button.link>
+            </div>
+        @endunless
     </div>
+
+    @if($search)
     <div class="absolute bg-white border text-sm shadow-xl rounded w-72 mt-4 ">
         <ul>
-            @foreach ($contacts as $contact)
+            @forelse ($contacts as $contact)
             <li class="border-b border-gray-300">
-                <x-button.link href="#" class=" text-left block hover:bg-gray-100 w-full p-3">{{ $contact->name }}</x-button.link>
+                <x-button.link wire:click="selectContact({{ $contact->id }})"
+                               class="text-left block hover:bg-gray-100 w-full p-3">
+                    {{ $contact->name }}
+                </x-button.link>
             </li>
-            @endforeach
-            <li class="border-b border-gray-300">
-                <a href="#" class="block hover:bg-gray-100 p-3">TeddyBear</a>
-            </li>
-            <li class="border-b border-gray-300">
-                <a href="#" class="block hover:bg-gray-100 p-3">TeddyBear</a>
-            </li>
+            @empty
+                <li class="border-b border-gray-300">
+                    <div class="p-4">No Users Found...</div>
+                </li>
+            @endforelse
         </ul>
     </div>
+    @endif
 </div>
